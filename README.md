6859672a0a2f02663fb2a52cff9699b013bbf445
Daniel Bigos <daniel.bigos@icloud.com>
2024-11-19 11:19:26 +0100

    docs: update README

--- README.md
+++ README.md
@@ -37,7 +37,12 @@ cargo stylus check
 With the following command you can deploy it to an Arbitrum chain
 
 ```shell
-cargo stylus deploy --endpoint $RPC_URL --private-key $PRIVATE_KEY
+cargo stylus deploy --private-key $PRIVATE_KEY -e $RPC_URL --no-verify
+```
+
+For example
+```shell
+cargo stylus deploy --private-key=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -e=http://localhost:8547 --no-verify
 ```
 
 ## Tests
@@ -61,6 +66,22 @@ cargo stylus export-abi
 ## How to run a local dev node
 
 Instructions to setup a local dev node can be found [here](https://docs.arbitrum.io/run-arbitrum-node/run-nitro-dev-node).
+
+Alternatively, you can use the bash script available [nitro-testnode.sh](/scripts/nitro-testnode.sh)
+
+```shell
+./scripts/nitro-testnode.sh -d -i
+```
+
+If you need to have some testnet tokens, you can use this script
+```shell
+./nitro-testnode/test-node.bash script send-l2 --to address_<address> --ethamount <amount>
+```
+
+For example
+```shell
+./nitro-testnode/test-node.bash script send-l2 --to address_0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --ethamount 5
+```
 
 ## Solidity Interface
 
--- scripts/nitro-testnode.sh
+++ scripts/nitro-testnode.sh
@@ -2,17 +2,65 @@
 
 MYDIR=$(realpath "$(dirname "$0")")
 cd "$MYDIR" || exit
+
+HAS_INIT=false
+HAS_DETACH=false
+
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    -i|--init)
+      HAS_INIT=true
+      shift
+      ;;
+    -d|--detach)
+      HAS_DETACH=true
+      shift
+      ;;
+    -q|--quit)
+      NITRO_CONTAINERS=$(docker container ls -q --filter name=nitro-testnode)
+
+      if [ -z "$NITRO_CONTAINERS" ]; then
+          echo "No nitro-testnode containers running"
+      else
+          docker container stop $NITRO_CONTAINERS || exit
+      fi
+
+      exit 0
+      ;;
+    *)
+      echo "OPTIONS:"
+      echo "-i|--init:         clone repo and init nitro test node"
+      echo "-d|--detach:       setup nitro test node in detached mode"
+      echo "-q|--quit:         shutdown nitro test node docker containers"
+      exit 0
+      ;;
+  esac
+done
+
 TEST_NODE_DIR="$MYDIR/../nitro-testnode"
-cd ../
+if [ ! -d "$TEST_NODE_DIR" ]; then
+  HAS_INIT=true
+fi
 
-# Clone nitro-testnode
-git clone --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
-cd ./nitro-testnode || exit
-# `release` branch.
-git checkout 2c2a97c2608ab0c83f2322af2a0dc2bf0b15ea31 || exit
+if $HAS_INIT
+then
+  cd "$MYDIR" || exit
+  cd ..
 
-# Initialize test node
-./test-node.bash --no-run --init || exit
+  git clone --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
+  cd ./nitro-testnode || exit
+  git pull origin release --recurse-submodules
+  git checkout e28b14472a7d46c42ff2be14f68ee08e2cbdad0f || exit
 
-# Start with detached mode
-./test-node.bash --detach
+  ./test-node.bash --no-run --init || exit
+fi
+
+
+cd "$TEST_NODE_DIR" || exit
+if $HAS_DETACH
+then
+  ./test-node.bash --detach
+else
+  ./test-node.bash
+fi
